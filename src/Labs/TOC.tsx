42e222816de549340d10b6cc399d2ed0f94c4333
obiyang <byanglol1206@gmail.com>
2025-02-03 23:44:02 -0800

    lab index updated

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -4,23 +4,20 @@ export default function TOC() {
   return (
     <Nav variant="pills">
       <Nav.Item>
-        <Nav.Link href="#/Labs">Lab 1</Nav.Link>
+        <Nav.Link href="#/Labs/Lab1">Lab 1</Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="#/Labs/Lab1">Lab 1</Nav.Link>
-        </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Labs/Lab2">Lab 2</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Labs/Lab2">Lab 3</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="https://github.com/obiyang/kambaz-react-web-app.git">My GitHub</Nav.Link>
-     </Nav.Item>
-   </Nav>
-);}
- 
+        <Nav.Link href="#/Labs/Lab2">Lab 2</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#/Labs/Lab3">Lab 3</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="https://github.com/obiyang/kambaz-react-web-app.git">My GitHub</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+}
