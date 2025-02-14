b8124501002dea0c3ef06b60aa0058584289de55
obiyang <byanglol1206@gmail.com>
2025-02-14 13:36:23 -0800

    first updated

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,15 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+
+
 export default function Lab3() {
     return (
       <div>
         <h2>Lab 3</h2>
+        <VariablesAndConstants />
+        <VariableTypes />
+        <BooleanVariables />
       </div>
   );}
   
